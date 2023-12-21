216c2996d7e0b598a7fe9529fc46e4684d891d59
putriadnda19 <120323777+putriadnda19@users.noreply.github.com>
2023-12-21 12:18:20 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -6,7 +6,7 @@ model = pickle.load(open('knn-paru-paru.sav', 'rb'))
 st.title('Estimasi Pasien Yang Menderita Kanker Paru-Paru')
 
 AGE = st.number_input('Input umur pasien')
-st.wite('Note: 1=NO, 2=YES')
+st.write('Note: 1=NO, 2=YES')
 SMOKING = st.number_input('Apakah pasien merokok?')
 YELLOW_FINGERS = st.number_input('Apakah pasien jari pasien kuning?')
 ANXIETY = st.number_input('Apakah pasien mempunyai kecemasan berlebih?')
