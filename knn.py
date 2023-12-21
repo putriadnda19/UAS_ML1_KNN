ee85e5e805d8a4b0e53d3262fae95b56a2ae3aaa
putriadnda19 <120323777+putriadnda19@users.noreply.github.com>
2023-12-21 19:03:51 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -5,16 +5,37 @@ model = pickle.load(open('knn-paru-paru.sav', 'rb'))
 
 st.title('Estimasi Pasien Yang Menderita Kanker Paru-Paru')
 
-AGE = st.number_input('Input umur pasien')
-SMOKING = st.number_input('Apakah pasien merokok?')
-YELLOW_FINGERS = st.number_input('Apakah pasien jari pasien kuning?')
-ANXIETY = st.number_input('Apakah pasien mempunyai kecemasan berlebih?')
-PEER_PRESSURE = st.number_input('Apakah pasien mempunyai tekanan dari teman sebaya?')
-COUGHING = st.number_input('Apakah pasien batuk-batuk?')
-SHORTNESS_OF_BREATH = st.number_input('Apakah pasien sesak nafas?')
-SWALLOWING_DIFFICULTY = st.number_input('Apakah pasien kesulitan menelan?')
-CHEST_PAIN = st.number_input('Apakah pasien nyeri dada?')
-CHRONIC_DISEASE = st.number_input('Apakah pasien mempunyai penyakit kronis?')
+with col1 :
+    AGE = st.number_input('Input umur pasien')
+
+with col2 :
+    SMOKING = st.number_input('Apakah pasien merokok?')
+
+with col1 :
+    YELLOW_FINGERS = st.number_input('Apakah pasien jari pasien kuning?')
+
+with col2:
+    ANXIETY = st.number_input('Apakah pasien mempunyai kecemasan berlebih?')
+
+with col1 :
+    PEER_PRESSURE = st.number_input('Apakah pasien mempunyai tekanan dari teman sebaya?')
+
+with col2 :
+    COUGHING = st.number_input('Apakah pasien batuk-batuk?')
+
+with col1 :
+    SHORTNESS_OF_BREATH = st.number_input('Apakah pasien sesak nafas?')
+
+with col2 :
+    SWALLOWING_DIFFICULTY = st.number_input('Apakah pasien kesulitan menelan?')
+
+with col1 :
+    CHEST_PAIN = st.number_input('Apakah pasien nyeri dada?')
+
+with col2 :
+    CHRONIC_DISEASE = st.number_input('Apakah pasien mempunyai penyakit kronis?')
+
+with col1 :
 WHEEZING = st.number_input('Apakah pasien mengi (Napas Berbunyi)?')
 
 predict = ''
