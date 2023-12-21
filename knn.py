e715753be52371eff404fa39d0c0f39adbcecc65
putriadnda19 <120323777+putriadnda19@users.noreply.github.com>
2023-12-21 18:29:39 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,26 +1,49 @@
-import pickle 
-import streamlit as st 
+# app.py
 
-model = pickle.load(open('knn-paru-paru.sav', 'rb'))
+import streamlit as st
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-st.title('Estimasi Pasien Yang Menderita Kanker Paru-Paru')
+# Fungsi untuk melatih model KNN
+def train_knn_model(X_train, y_train, n_neighbors=3):
+    model = KNeighborsClassifier(n_neighbors=n_neighbors)
+    model.fit(X_train, y_train)
+    return model
 
-AGE = st.number_input('Input umur pasien')
-SMOKING = st.number_input('Apakah pasien merokok?')
-YELLOW_FINGERS = st.number_input('Apakah pasien jari pasien kuning?')
-ANXIETY = st.number_input('Apakah pasien mempunyai kecemasan berlebih?')
-PEER_PRESSURE = st.number_input('Apakah pasien mempunyai tekanan dari teman sebaya?')
-COUGHING = st.number_input('Apakah pasien batuk-batuk?')
-SHORTNESS_OF_BREATH = st.number_input('Apakah pasien sesak nafas?')
-SWALLOWING_DIFFICULTY = st.number_input('Apakah pasien kesulitan menelan?')
-CHEST_PAIN = st.number_input('Apakah pasien nyeri dada?')
-CHRONIC_DISEASE = st.number_input('Apakah pasien mempunyai penyakit kronis?')
-WHEEZING = st.number_input('Apakah pasien mengi (Napas Berbunyi)?')
+# Fungsi untuk mengevaluasi model
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
 
-predict = ''
+def main():
+    st.title("Aplikasi KNN dengan Streamlit")
 
-if st.button('Estimasi ', type="primary"):
-    predict = model.predict(
-        [[AGE, SMOKING, YELLOW_FINGERS, ANXIETY, PEER_PRESSURE, COUGHING, SHORTNESS_OF_BREATH, SWALLOWING_DIFFICULTY, CHEST_PAIN, CHRONIC_DISEASE, WHEEZING]]
-    )
-    st.write('Apakah orang-orang dengan karakteristik tersebut memiliki kanker paru-paru atau tidak? : ', predict)
+    # Tambahkan elemen-elemen UI
+    st.sidebar.header("Pengaturan Model")
+    k_value = st.sidebar.slider("Jumlah Tetangga (k)", 1, 10, 3)
+
+    # Muat dataset (ganti dengan dataset Anda)
+    # Misalnya, Anda dapat menggunakan dataset iris untuk contoh
+    from sklearn.datasets import load_iris
+    iris = load_iris()
+    X = pd.DataFrame(iris.data, columns=iris.feature_names)
+    y = iris.target
+
+    # Pisahkan data menjadi data pelatihan dan pengujian
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Latih model
+    model = train_knn_model(X_train, y_train, n_neighbors=k_value)
+
+    # Evaluasi model
+    accuracy = evaluate_model(model, X_test, y_test)
+
+    # Tampilkan hasil evaluasi
+    st.sidebar.subheader("Hasil Evaluasi Model")
+    st.sidebar.write(f"Akurasi: {accuracy:.2%}")
+
+if __name__ == "__main__":
+    main()
