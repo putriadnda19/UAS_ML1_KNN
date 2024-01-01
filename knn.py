3dd840ba519f06468bb199d4cdc28144c571b8a7
putriadnda19 <120323777+putriadnda19@users.noreply.github.com>
2024-01-01 11:20:29 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -5,17 +5,37 @@ model = pickle.load(open('knn-paru-paru.sav', 'rb'))
 
 st.title('Estimasi Pasien Yang Menderita Kanker Paru-Paru')
 
-AGE = st.number_input('Input umur pasien')
-SMOKING = st.number_input('Apakah pasien merokok?')
-YELLOW_FINGERS = st.number_input('Apakah pasien jari pasien kuning?')
-ANXIETY = st.number_input('Apakah pasien mempunyai kecemasan berlebih?')
-PEER_PRESSURE = st.number_input('Apakah pasien mempunyai tekanan dari teman sebaya?')
-COUGHING = st.number_input('Apakah pasien batuk-batuk?')
-SHORTNESS_OF_BREATH = st.number_input('Apakah pasien sesak nafas?')
-SWALLOWING_DIFFICULTY = st.number_input('Apakah pasien kesulitan menelan?')
-CHEST_PAIN = st.number_input('Apakah pasien nyeri dada?')
-CHRONIC_DISEASE = st.number_input('Apakah pasien mempunyai penyakit kronis?')
-WHEEZING = st.number_input('Apakah pasien mengi (Napas Berbunyi)?')
+AGE = st.slider('**Input umur pasien**', 1, 80)
+
+SMOKING = st.radio('**Apakah pasien merokok?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+YELLOW_FINGERS = st.radio('**Apakah pasien jari pasien kuning?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+ANXIETY = st.radio('**Apakah pasien mempunyai kecemasan berlebih?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+PEER_PRESSURE = st.radio('**Apakah pasien mempunyai tekanan dari teman sebaya?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+COUGHING = st.radio('**Apakah pasien batuk-batuk?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+SHORTNESS_OF_BREATH = st.radio('**Apakah pasien sesak nafas?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+SWALLOWING_DIFFICULTY = st.radio('**Apakah pasien kesulitan menelan?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+CHEST_PAIN = st.radio('**Apakah pasien nyeri dada?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+CHRONIC_DISEASE = st.radio('**Apakah pasien mempunyai penyakit kronis?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
+
+WHEEZING = st.radio('**Apakah pasien mengi (Napas Berbunyi)?**', ["1", "2"])
+st.write('**Note**: 1 = NO, 2 = YES')
 
 predict = ''
 
