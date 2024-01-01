78c4ee04335d01340a92f778cc37b60de734029c
putriadnda19 <120323777+putriadnda19@users.noreply.github.com>
2024-01-01 12:22:08 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -12,7 +12,9 @@ with col1 :
     AGE = st.slider('Input umur pasien', 1, 80)
 
 with col2 :
-    SMOKING = st.number_input('Apakah pasien merokok?')
+    SMOKING = st.radio(
+    "Apakah pasien merokok?",
+    ["YES", "NO"])
 
 with col1 :
     YELLOW_FINGERS = st.number_input('Apakah pasien jari pasien kuning?')
