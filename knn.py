d9613f0a21e956f26fc0fdfd79540b0e1d8f17cb
putriadnda19 <120323777+putriadnda19@users.noreply.github.com>
2024-01-01 11:32:08 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -42,7 +42,7 @@ with col1 :
 
 predict = ''
 
-if st.button('Estimasi '):
+if st.button('Estimasi ', type="primary"):
     predict = model.predict(
         [[AGE, SMOKING, YELLOW_FINGERS, ANXIETY, PEER_PRESSURE, COUGHING, SHORTNESS_OF_BREATH, SWALLOWING_DIFFICULTY, CHEST_PAIN, CHRONIC_DISEASE, WHEEZING]]
     )
